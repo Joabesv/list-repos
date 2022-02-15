a3d71da231b7b3dc48d95f81a372617854e11dcd
Joabesv <joabevarjao123@gmail.com>
2022-02-14 22:55:21 -0300

    creating a component and rendering it

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,46 @@
+const repositoryName = 'Unform2';
+
+export function RepositoryList() {
+  return (
+    <section className='repository-list'>
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href=''>Acessar Repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href=''>Acessar Repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href=''>Acessar Repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href=''>Acessar Repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href=''>Acessar Repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
