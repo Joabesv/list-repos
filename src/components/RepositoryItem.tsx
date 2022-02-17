c0b68ec32fcbf9e250165ad64934752d5fbd2f94
Joabesv <joabevarjao123@gmail.com>
2022-02-16 22:28:19 -0300

    finished transitioning, liked the process

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,8 +1,11 @@
-export function RepositoryItem(props) {
+import { RepositoryProps } from '../../types/repository';
+
+export function RepositoryItem(props: RepositoryProps) {
   return (
     <li>
       <strong>{props?.repository?.name}</strong>
       <p>{props?.repository?.description}</p>
+
       <a href={props?.repository?.html_url}>Acessar Repositório</a>
     </li>
   );
---
+++ types/repository.ts
@@ -0,0 +1,7 @@
+export interface RepositoryProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+}
